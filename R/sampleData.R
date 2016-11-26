9ce04ee966c3539e25b36d8e7093911bc34cfb76
Oscar Garcia-Cabrejo <khaors@gmail.com>
2016-11-25 23:19:21 -0500

    doc files added. Sample data added.

---
+++ R/sampleData.R
@@ -0,0 +1,13 @@
+#' Numbers and Letters
+#' 
+#'  A sample data set containing 10 numbers and letters with two variables:
+#'  
+#'  \itemize{
+#'    \item Num: A number.
+#'    \item Letter: An upper case letter (A to J)
+#'  
+#'  }  
+#'  
+#'  @format A data frame with 10 rows and 2 variables
+#'  @source Created as sample
+"sampleData"
